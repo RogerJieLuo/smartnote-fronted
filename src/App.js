7b958c8a8c8c7da0fb419bedd756400dcffc4407
jieluo <jieluo122@gmail.com>
2021-07-04 21:19:34 -0700

    added auth

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./route";
 import GlobalStyle from "./globalStyle";
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
+      <AmplifySignOut />
     </Fragment>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
